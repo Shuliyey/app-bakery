4bc09e1d20e187ed6559e2bb987172c1533647f7
叶泽宇(Zéyǔ Yè) <shuliyey@gmail.com>
2019-08-30 19:57:20 +1000

    :bug: ansible base role yml error missing escape character; :sparkles: packer base add VPC_NO_PROXY environment variable; :sparkles: packer base main.yaml playbook, environment variable in playbook to run roles;

--- packer/base/scripts/dep.d/aliyun.sh
+++ packer/base/scripts/dep.d/aliyun.sh
@@ -4,7 +4,16 @@ echo -e "ALIYUNPYCLI_VERSION = $(clr_cyan ${ALIYUNPYCLI_VERSION})"
 echo -e "YQ_VERSION = $(clr_cyan ${YQ_VERSION})"
 echo -e "TERMTOSVG_VERSION = $(clr_cyan ${TERMTOSVG_VERSION})"
 
-export DEBIAN_FRONTEND=noninteractive
-apt-get update
-apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
-apt-get install -y python language-pack-en
+os_id=$(cat /etc/os-release | grep -E "^\s*ID\s*=\s*\".+\"" | sed -E "s/\s*ID\s*=\s*\"(.+)\"/\1/g")
+
+if [ "${os_id}" == "ubuntu" ]; then
+  export DEBIAN_FRONTEND=noninteractive
+  apt-get update
+  apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
+  apt-get install -y python language-pack-en
+fi
+
+if [ "${os_id}" == "amzn" ]; then
+  sudo yum update -y
+  sudo yum install -y python
+fi
--- packer/base/scripts/dep.d/aws.sh
+++ packer/base/scripts/dep.d/aws.sh
@@ -2,7 +2,16 @@ echo -e "AWSCLI_VERSION = $(clr_cyan ${AWSCLI_VERSION})"
 echo -e "YQ_VERSION = $(clr_cyan ${YQ_VERSION})"
 echo -e "TERMTOSVG_VERSION = $(clr_cyan ${TERMTOSVG_VERSION})"
 
-export DEBIAN_FRONTEND=noninteractive
-apt-get update
-apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
-apt-get install -y python language-pack-en
+os_id=$(cat /etc/os-release | grep -E "^\s*ID\s*=\s*\".+\"" | sed -E "s/\s*ID\s*=\s*\"(.+)\"/\1/g")
+
+if [ "${os_id}" == "ubuntu" ]; then
+  export DEBIAN_FRONTEND=noninteractive
+  apt-get update
+  apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
+  apt-get install -y python language-pack-en
+fi
+
+if [ "${os_id}" == "amzn" ]; then
+  sudo yum update -y
+  sudo yum install -y python
+fi
