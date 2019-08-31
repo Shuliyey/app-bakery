0cc73ce847ae8884cd09f40f38c066c5e60f8393
叶泽宇(Zéyǔ Yè) <shuliyey@gmail.com>
2019-08-31 20:59:59 +1000

    :bug: ansible base syntax issues in conditional statement, add handling for .bash_profile in /etc/skel

--- packer/base/dep.d/aliyun.sh
+++ packer/base/dep.d/aliyun.sh
@@ -10,9 +10,9 @@ local DEFAULT_VSWITCH_ID=""
 local DEFAULT_VPC_HTTP_PROXY=""
 local DEFAULT_VPC_HTTPS_PROXY=""
 local DEFAULT_VPC_NO_PROXY=""
-local DEFAULT_ALIYUN_CLOUDMONITOR_VERSION=1.2.11
+local DEFAULT_ALIYUN_CLOUDMONITOR_VERSION="1.2.11"
 local DEFAULT_ALIYUNGOCLI_VERSION=$(curl --silent "https://api.github.com/repos/aliyun/aliyun-cli/releases/latest" | jq -r ".tag_name" | sed -E 's/^v([0-9\.]+).*$/\1/g')
-local DEFAULT_ALIYUNPYCLI_VERSION=2.1.9
+local DEFAULT_ALIYUNPYCLI_VERSION="2.1.9"
 local DEFAULT_YQ_VERSION=$(curl --silent "https://api.github.com/repos/rmescandon/yq/releases/latest" | jq -r ".tag_name" | sed -E 's/^v([0-9\.]+).*$/\1/g')
 local DEFAULT_TERMTOSVG_VERSION=$(curl --silent "https://api.github.com/repos/nbedos/termtosvg/releases/latest" | jq -r ".tag_name" | sed -E 's/^v([0-9\.]+).*$/\1/g')
 
