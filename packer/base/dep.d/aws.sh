f4846e661b86d88c5a20ead1ca54558fbfd40a5b
叶泽宇(Zéyǔ Yè) <shuliyey@gmail.com>
2019-09-02 16:48:36 +1000

    :wrench: packer base dep add AWSCLI_VERSION optional env

--- packer/base/dep.d/aws.sh
+++ packer/base/dep.d/aws.sh
@@ -10,8 +10,9 @@ local DEFAULT_SUBNET_ID=""
 local DEFAULT_VPC_HTTP_PROXY=""
 local DEFAULT_VPC_HTTPS_PROXY=""
 local DEFAULT_VPC_NO_PROXY=""
+local DEFAULT_AWSCLI_VERSION=""
 local DEFAULT_YQ_VERSION=$(curl --silent "https://api.github.com/repos/mikefarah/yq/releases/latest" | jq -r ".tag_name" | sed -E 's/^v([0-9\.]+).*$/\1/g')
 local DEFAULT_TERMTOSVG_VERSION=$(curl --silent "https://api.github.com/repos/nbedos/termtosvg/releases/latest" | jq -r ".tag_name" | sed -E 's/^v([0-9\.]+).*$/\1/g')
 
 local ENVS=("AWS_ACCESS_KEY_ID" "AWS_SECRET_ACCESS_KEY" "AWS_DEFAULT_REGION")
-local OPTIONAL_ENV=("APP" "DESCRIPTION" "DEBUG" "AWS_SESSION_TOKEN" "BUILD_VERSION" "BUILD_HOST" "COPY_REGIONS" "VPC_ID" "SUBNET_ID" "VPC_HTTP_PROXY" "VPC_HTTPS_PROXY" "VPC_NO_PROXY" "YQ_VERSION" "TERMTOSVG_VERSION")
+local OPTIONAL_ENV=("APP" "DESCRIPTION" "DEBUG" "AWS_SESSION_TOKEN" "BUILD_VERSION" "BUILD_HOST" "COPY_REGIONS" "VPC_ID" "SUBNET_ID" "VPC_HTTP_PROXY" "VPC_HTTPS_PROXY" "VPC_NO_PROXY" "AWSCLI_VERSION" "YQ_VERSION" "TERMTOSVG_VERSION")
